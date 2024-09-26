8603c55f88a0bf7a00a80acbf42bbc10f5e462f7
vanka334 <isip_i.d.kuzin@mpt.ru>
2024-09-26 23:21:57 +0300

    Revert "Тестовое задание на стажировку"
    This reverts commit e337b43ee68a850b94d560b312927276b9d261aa.

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-
-from api.api import ProductViewSet
-
-
-router = DefaultRouter()
-router.register(r'products', ProductViewSet)
-urlpatterns = router.urls
--- mainSite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from mainSite import views
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='products')
-]
